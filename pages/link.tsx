f5021cb4b3cf46c438166bab93f87d37c38b36d7
alexhughesef <alex.hughes@ef.com>
2020-06-19 16:34:46 -0400

    test client side navigation with dynamic page

--- pages/link.tsx
+++ pages/link.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default () => {
     return (
-        <Link href="/destination">
+        <Link href="/[destination]/here" as="/123/here">
             go to destination
         </Link>
     )
